067ebd998f94e5536dacab95df239aca8a4693de
Will Bradley <bradley.will@gmail.com>
2013-04-29 22:55:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyPaypalExample::Application.config.secret_token = 'e03b852d4fbde8f202cce8252c3189ce6ae07bafa8134b8fbe5d0a96290a737cf0fa0b0eca04e949555ddd191fbcfbfc8f8dd42e5cb30b703abb242ceff85c83'
